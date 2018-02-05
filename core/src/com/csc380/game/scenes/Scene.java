249c274c6bf1708363f652cf747fe17efe25a72a
Ye Bhone Myat <ymyat@oswego.edu>
2018-02-04 19:05:27 -0500

    Added methods render and update to Scenes.java

--- core/src/com/csc380/game/scenes/Scene.java
+++ core/src/com/csc380/game/scenes/Scene.java
@@ -1,10 +1,24 @@
 package com.csc380.game.scenes;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+
 /**
  * This class is an interface for creating more scenes.
  */
-public abstract class Scene {
+public abstract class Scene implements Disposable {
 
     private static final String TAG = Scene.class.getName();
+
+    /**
+     * Updates the physics, character positions, etc.
+     */
+    public abstract void update();
+
+    /**
+     * Renders the scenes to be drawn
+     * @param batch where the scene will be rendered
+     */
+    public abstract void render(SpriteBatch batch);
 
 }
