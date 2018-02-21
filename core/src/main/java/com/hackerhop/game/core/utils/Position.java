f34c6624cafbbb80889d76c8e11d3b33e49ffeab
nicovank <nvankemp@gmail.com>
2018-02-20 20:09:30 -0500

    Added Position utility class.

---
+++ core/src/main/java/com/hackerhop/game/core/utils/Position.java
@@ -0,0 +1,30 @@
+package com.hackerhop.game.core.utils;
+
+public class Position {
+    private double x, y;
+
+    public Position(double x, double y) {
+        setPosition(x, y);
+    }
+
+    public void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double setX() {
+        return x;
+    }
+
+    public double setY() {
+        return y;
+    }
+}
