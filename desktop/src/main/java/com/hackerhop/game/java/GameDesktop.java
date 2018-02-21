a99397d8e25ac5f50361c7957180fdb6f10cc545
nicovank <nvankemp@gmail.com>
2018-02-20 19:59:05 -0500

    Changed resolution.

--- desktop/src/main/java/com/hackerhop/game/java/GameDesktop.java
+++ desktop/src/main/java/com/hackerhop/game/java/GameDesktop.java
@@ -8,6 +8,8 @@ import com.hackerhop.game.core.Game;
 public class GameDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 540;
 		new LwjglApplication(new Game(), config);
 	}
 }
