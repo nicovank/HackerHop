9415744f18cf336f9b731b381fc0ae708869d9c8
nicovank <nvankemp@gmail.com>
2018-02-21 17:16:56 -0500

    Added basic Game Scene logic.

---
+++ core/src/main/java/com/hackerhop/game/core/scenes/GameScene.java
@@ -0,0 +1,69 @@
+package com.hackerhop.game.core.scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.hackerhop.game.core.Game;
+import org.jbox2d.dynamics.World;
+
+/**
+ * This scene is the "main" game, with the scrolling platforms and the player.
+ */
+public class GameScene extends Scene {
+
+    // Our physics world
+    private World world;
+
+    // Frame time accumulator
+    private float accumulator = 0.0f;
+
+    // Some world constants
+    private static final float TIME_STEP = 1 / 60f;
+    private static final int VELOCITY_ITERATIONS = 2;
+    private static final int POSITION_ITERATIONS = 6;
+
+    /**
+     * Creates a new Game Scene.
+     * TODO: Instantiate a new player, some platforms, etc.
+     *
+     * @param controller The Game controller. Used when we need to change scenes for example.
+     */
+    public GameScene(Game controller) {
+        super(controller);
+    }
+
+    /**
+     * Runs Box2D's physics step, making the objects move if needed.
+     * See https://github.com/libgdx/libgdx/wiki/box2d#stepping-the-simulation
+     */
+    @Override
+    public void update() {
+
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        float frameTime = Math.min(deltaTime, 0.25f);
+        accumulator += frameTime;
+
+        while (accumulator > TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            accumulator -= TIME_STEP;
+        }
+    }
+
+    /**
+     * We basically need to render each body in the game, as well as a background,
+     * and other UI elements (score, lives, etc.)
+     *
+     * @param batch where the scene will be rendered
+     */
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    /**
+     * Disposes of all the textures and other objects used in the scene.
+     */
+    @Override
+    public void dispose() {
+
+    }
+}
