6bff66c1006235da71bac71d68cdbada5b84fb1f
nicovank <nvankemp@gmail.com>
2018-02-21 17:20:14 -0500

    Format.

--- core/src/main/java/com/hackerhop/game/core/Game.java
+++ core/src/main/java/com/hackerhop/game/core/Game.java
@@ -14,59 +14,59 @@ import com.hackerhop.game.core.scenes.Scene;
  */
 public class Game extends ApplicationAdapter {
 
-	// We will have this TAG in every class to easily use LibGDX's logging system.
-	private static final String TAG = Game.class.getName();
+    // We will have this TAG in every class to easily use LibGDX's logging system.
+    private static final String TAG = Game.class.getName();
 
-	private SpriteBatch batch;
-	private Scene currentScene;
+    private SpriteBatch batch;
+    private Scene currentScene;
 
-	/**
-	 * Entry point of the application.
-	 */
-	@Override
-	public void create () {
+    /**
+     * Entry point of the application.
+     */
+    @Override
+    public void create() {
 
-		// Set application to log everything.
-		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Set application to log everything.
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
-		batch = new SpriteBatch();
+        batch = new SpriteBatch();
 
-		// Start the game with the menu
-		currentScene = new Menu(this);
+        // Start the game with the menu
+        currentScene = new Menu(this);
 
-		// To log information or errors use:
+        // To log information or errors use:
 
-		// Gdx.app.log(TAG, "This is an info log.");
-		// Gdx.app.debug(TAG, "This is a debug log.");
-		// Gdx.app.error(TAG, "This is an error log.");
-	}
+        // Gdx.app.log(TAG, "This is an info log.");
+        // Gdx.app.debug(TAG, "This is a debug log.");
+        // Gdx.app.error(TAG, "This is an error log.");
+    }
 
-	/**
-	 * "Main" rendering, renders the current scene.
-	 */
-	@Override
-	public void render () {
-		// Update current scene
-		currentScene.update();
+    /**
+     * "Main" rendering, renders the current scene.
+     */
+    @Override
+    public void render() {
+        // Update current scene
+        currentScene.update();
 
-		// Clear frame
-		Gdx.gl.glClearColor(1, .5f, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Clear frame
+        Gdx.gl.glClearColor(1, .5f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// Draw next frame
-		batch.begin();
-		// Draw sprites and textures (or the scene) to the batch.
-		// Draws the current scene.
-		currentScene.render(batch);
-		batch.end();
-	}
+        // Draw next frame
+        batch.begin();
+        // Draw sprites and textures (or the scene) to the batch.
+        // Draws the current scene.
+        currentScene.render(batch);
+        batch.end();
+    }
 
-	/**
-	 * Disposes of the batch and the current scene.
-	 */
-	@Override
-	public void dispose () {
-		batch.dispose();
-		currentScene.dispose();
-	}
+    /**
+     * Disposes of the batch and the current scene.
+     */
+    @Override
+    public void dispose() {
+        batch.dispose();
+        currentScene.dispose();
+    }
 }
