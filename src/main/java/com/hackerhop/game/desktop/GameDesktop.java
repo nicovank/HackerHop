6bd7e52a3c98eef341bf98ec54b6d1e217358f58
nicovank <nvankemp@gmail.com>
2018-03-06 12:53:59 -0500

    Removed Maven profile. Makes structure easier.

--- src/main/java/com/hackerhop/game/desktop/GameDesktop.java
+++ src/main/java/com/hackerhop/game/desktop/GameDesktop.java
@@ -1,4 +1,4 @@
-package com.hackerhop.game.java;
+package com.hackerhop.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
