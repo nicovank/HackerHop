231c8a193aefe1d47e016907e3626ee41a17b378
Kate Gordon <gordonkatie01@gmail.com>
2018-03-08 14:00:29 -0500

    More work on distance test

--- src/test/java/com/hackerhop/game/core/objects/PlatformTest.java
+++ src/test/java/com/hackerhop/game/core/objects/PlatformTest.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlatformTest {
-//    @Test
-//    void distanceTest() {
-//        World w = new World(new Vec2(0, -50));
-//        float x = 10;
-//        float y = 20;
-//        float x2 = 30;
-//        float y2 = 40;
-//        Platform a = new Platform(x, y, w);
-//        Platform b = new Platform(x2, y2, w);
-//        a.distanceTo(b);
-//        //distance should equal approx 28.28
-//
-//    }
+    @Test
+    void distanceTest() {
+        World w = new World(new Vec2(0, -50));
+        float x = 10;
+        float y = 20;
+        float x2 = 30;
+        float y2 = 40;
+        Platform a = new Platform(x, y, w);
+        Platform b = new Platform(x2, y2, w);
+        double distance = Math.sqrt(Math.pow((30 - 10), 2) + Math.pow((40 - 20), 2));
+        assertEquals(distance, b.distanceTo(a));
+    }
 }
