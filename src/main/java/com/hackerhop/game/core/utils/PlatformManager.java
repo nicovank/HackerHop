daaddf9d71878bc8c01659f1b2dda419c60d4ef4
Ye Bhone Myat <ymyat@oswego.edu>
2018-03-08 13:02:42 -0500

    broken, but working on platform generation

---
+++ src/main/java/com/hackerhop/game/core/utils/PlatformManager.java
@@ -0,0 +1,17 @@
+package com.hackerhop.game.core.utils;
+
+import com.hackerhop.game.core.objects.Platform;
+
+public class PlatformManager {
+
+    private final float gridSeparation = 75;
+    private final int xCount = (int) Math.floor(540/gridSeparation);
+    private final int yCount = (int) Math.floor(720/gridSeparation);
+
+    public PlatformManager(){}
+
+    public void generateGrids(){
+        
+    }
+
+}
