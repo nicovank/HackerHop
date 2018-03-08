c8a22acdcce922bf853df8b5f2e2e016556f93d9
Ye Bhone Myat <ymyat@oswego.edu>
2018-03-08 14:08:51 -0500

    Platforms working now

--- src/main/java/com/hackerhop/game/core/utils/PlatformManager.java
+++ src/main/java/com/hackerhop/game/core/utils/PlatformManager.java
@@ -4,10 +4,11 @@ import com.hackerhop.game.core.objects.Platform;
 import org.jbox2d.dynamics.World;
 
 import java.util.HashSet;
+import java.util.Random;
 
 public class PlatformManager {
 
-    private final float gridSeparation = 150;
+    private final float gridSeparation = 25;
     private final int xCount = (int) Math.floor(540/gridSeparation);
     private final int yCount = (int) Math.floor(720/gridSeparation);
     private Grid[][] grids = new Grid[xCount][yCount];
@@ -16,13 +17,19 @@ public class PlatformManager {
 
     public HashSet<Platform> generatePlatforms(World w){
         HashSet<Platform> h = new HashSet<>();
+        Random r = new Random();
 
         for (int i = 0; i < yCount; ++i){
             for (int j = 0; j < xCount; ++j){
-                Platform p = new Platform(gridSeparation * (j + 1), gridSeparation * (i + 1), w);
-                h.add(p);
+                if (r.nextBoolean()) {
+                    Platform p = new Platform(gridSeparation * (j), gridSeparation * (i), w);
+                    h.add(p);
+                }
             }
         }
+
+//        Platform p = new Platform(0, 0, w);
+//        h.add(p);
 
         return h;
     }
