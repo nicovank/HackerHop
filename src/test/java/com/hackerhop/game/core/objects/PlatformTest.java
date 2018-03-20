f2e7adfd29854418201534411cb0370a860291ec
Kate Gordon <gordonkatie01@gmail.com>
2018-03-20 13:58:48 -0400

    Changed format of platformtest, still need to fix nullpointer

--- src/test/java/com/hackerhop/game/core/objects/PlatformTest.java
+++ src/test/java/com/hackerhop/game/core/objects/PlatformTest.java
@@ -2,22 +2,37 @@ package com.hackerhop.game.core.objects;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlatformTest {
   //TODO: Figure out null pointer exception when instantiating a platform
-      @Test
+    Vec2 worldVec;
+    World w;
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    Platform b;
+    Platform a;
+    double distance;
+
+    @Before
+    void setUp(){
+        worldVec = new Vec2(0, -50);
+        w = new World(worldVec);
+        x1 = 10;
+        y1 = 20;
+        x2 = 30;
+        y2 = 40;
+        b = new Platform(x2, y2, w);
+        a = new Platform(x1, y1, w);
+        distance = Math.sqrt(Math.pow((30 - 10), 2) + Math.pow((40 - 20), 2));
+    }
+
+    @Test
     void distanceTest() {
-        Vec2 worldVec = new Vec2(0, -50);
-        World w = new World(worldVec);
-        float x1 = 10;
-        float y1 = 20;
-        float x2 = 30;
-        float y2 = 40;
-        //Platform b = new Platform(x2, y2, w);
-        //Platform a = new Platform(x1, y1, w);
-        double distance = Math.sqrt(Math.pow((30 - 10), 2) + Math.pow((40 - 20), 2));
-        assertEquals(distance, distance);
+        assertEquals(distance, b.distanceTo(a));
     }
 }
