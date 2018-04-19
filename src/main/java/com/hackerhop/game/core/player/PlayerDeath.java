7df17178a8ff997339fb6b597aee4dee79044a90
RSgroi <rsgroi@oswego.edu>
2018-04-19 13:24:36 -0400

    Fuck u nick

---
+++ src/main/java/com/hackerhop/game/core/player/PlayerDeath.java
@@ -0,0 +1,23 @@
+package com.hackerhop.game.core.player;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.hackerhop.game.core.graphics.GraphicsElement;
+import com.hackerhop.game.core.objects.PhysicalObject;
+import com.hackerhop.game.core.utils.Constants;
+
+public class PlayerDeath extends PhysicalObject implements GraphicsElement, Constants {
+    @Override
+    public void loadResources() {
+        
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
