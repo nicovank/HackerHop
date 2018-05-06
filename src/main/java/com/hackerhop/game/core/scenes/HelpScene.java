ccb89239bdafc4f2c46cf6dd46b864a57930524b
RSgroi <rsgroi@oswego.edu>
2018-05-06 16:40:36 -0400

    added to helpScene

--- src/main/java/com/hackerhop/game/core/scenes/HelpScene.java
+++ src/main/java/com/hackerhop/game/core/scenes/HelpScene.java
@@ -7,9 +7,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.hackerhop.game.core.MainController;
+import com.hackerhop.game.core.objects.Coin;
 import com.hackerhop.game.core.objects.Space;
 import com.hackerhop.game.core.player.Character;
 import com.hackerhop.game.core.utils.blinkers.SpriteBlinker;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
 
 //This will be between MainMenu and GameScene, explaining controls and other Game Mechanics
 //Will be very simple, The constructor carries over the Character picked in main menu to GameScene so it should be easily implemented.
@@ -18,7 +21,7 @@ public class HelpScene extends Scene {
     private static final String TAG = GameScene.class.getName();
     private final Character character;
     private Texture background;
-    private Space space = new Space(77, 50);
+    private Space space = new Space(77, 30);
     private Sprite player;
     private Texture clouds;
     private SpriteBlinker blinker1;
@@ -30,7 +33,10 @@ public class HelpScene extends Scene {
     private Sprite player2;
     private SpriteBlinker blinker5;
     private SpriteBlinker blinker6;
+    private SpriteBlinker blinker7;
     private Music music;
+    private Coin coin;
+    private World world = new World(new Vec2(0, -50));
 
     public HelpScene(MainController controller, Character character) {
         super(controller);
@@ -46,6 +52,7 @@ public class HelpScene extends Scene {
         blinker4.update();
         blinker5.update();
         blinker6.update();
+        blinker7.update();
         space.update();
     }
 
@@ -108,6 +115,7 @@ public class HelpScene extends Scene {
         blinker2 = new SpriteBlinker(arrow, 1f, .5f);
         blinker3 = new SpriteBlinker(arrowR, 1f, .5f);
         blinker4 = new SpriteBlinker(arrowR, 1f, .5f);
+        blinker7 = new SpriteBlinker(arrowR,1f,.5f);
 
 
         clouds = new Texture("background/cloud.png");
@@ -132,7 +140,8 @@ public class HelpScene extends Scene {
         player.setPosition(405, 630);
         blinker5 = new SpriteBlinker(player2,.75f,1f);
         blinker6 = new SpriteBlinker(player2,1f,.75f,false); //blinker 6 with new case
-
+        coin = new Coin(45,15, world);
+        coin.loadResources();
     }
 
     @Override
@@ -147,7 +156,11 @@ public class HelpScene extends Scene {
         blinker4.render(batch, 355, 325);
         blinker5.render(batch,407,400);
         blinker6.render(batch,407,352); //This one needs to start hidden, and alternate with blinker 5
+        blinker7.render(batch,380,145);
         player.draw(batch);
+        coin.update();
+        coin.render(batch);
+
         batch.end();
 
     }
@@ -161,12 +174,14 @@ public class HelpScene extends Scene {
         blinker1.dispose();
         blinker5.dispose();
         blinker6.dispose();
+        blinker7.dispose();
         clouds.dispose();
         background.dispose();
         player.getTexture().dispose();
         player2.getTexture().dispose();
         arrow.getTexture().dispose();
         arrowR.getTexture().dispose();
+        coin.dispose();
     }
 
 
